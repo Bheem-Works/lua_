51d5c750555374d350d80ad9b4d5f33902d18052
bheem-works <shokokomichan098@gmail.com>
2025-06-10 14:25:13 +0545

    lua intro

--- intro_lua.lua
+++ intro_lua.lua
@@ -1,4 +1,21 @@
 
+-- lua : lua is a procedural programming language used to develop the software, work with the machine learning and to create all sorts 
+-- games, from a basics text games all way to the vr games. It is high programming language multi paradigrim script design primarily for
+-- embedded used in applications; It is knowns for it's simplicity and the ease of integerations with other's lanugage like c, c++;
 
- -- This is the lua Programming langauge basics models.
-print("Hello Mom");
+-- Table of the content of the lua: 
+-- Data types in varaibles
+-- Maths
+-- string
+-- if statement
+-- loops 
+-- user inputs
+-- table
+-- functions 
+-- co-routine
+-- working with the files
+-- the os modules 
+-- the customs modules
+-- oop
+-- metamethods
+
