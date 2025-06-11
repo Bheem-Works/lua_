9989e792970afbe0ebb5d5d58a6b5a8dcaa513e7
bheem-works <shokokomichan098@gmail.com>
2025-06-12 00:07:45 +0545

    Math Commit

---
+++ Number.lua
@@ -0,0 +1,17 @@
+    local str = "11"
+    print(type(tonumber(str)));
+
+    -- To Power 
+    print(2^2);
+
+
+    -- Math libarary;
+    print(math.pi);
+    print(math.random());
+    print(math.randomseed(os.time()));
+    print(os.time());
+    print(math.min(10,2,3)); -- it give the minimum values 
+    print(math.floor(10.22022)) -- it gives the rounded values; 
+    print(math.sin(19));
+    print(math.abs(29.3883));
+    
