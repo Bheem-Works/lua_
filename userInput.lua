a7cd4c5ed68c377733f4b7d2b1ed0cb6fec04642
bheem-works <shokokomichan098@gmail.com>
2025-06-18 15:41:32 +0545

    lua userinput statement

--- userInput.lua
+++ userInput.lua
@@ -3,8 +3,39 @@
 -- 1. io.write() for write something's 
 -- 2. io.read() for read user inputs; 
 
-local userRead = io.write("what is you name?");
-print(userRead);
-local read = io.read();
-print("userInput:"..read);
+-- local userRead = io.write("what is you name?");
+-- print(userRead);
+-- local read = io.read();
+-- local readOne = io.read();
+-- print("userInputOne:"..readOne);
+-- print("userInput:"..read);
 
+-- Using the if statement in the lua; 
+
+-- local exOne = 1 
+-- local exTwo = 2
+-- local user_answer = io.write("what is the sum of the" ..exOne "+" ..exTwo "?")
+-- local user_read = io.read()
+-- local correct_answer = exOne + exTwo;
+-- -- use the if conidtions; 
+-- if user_read == correct_answer then
+--   print("Your answer is correct");
+-- end
+
+local read_user = io.write("What is your name ?");
+local readUser = io.read();
+if readUser == "vim" then
+    print("Yo!")
+else 
+    print("No Yo!");
+end
+
+-- for a loop  case;
+
+local user_loop = io.write("Enter the number:");
+local user_loop_read = io.read();
+print(user_loop_read);
+
+for i = user_loop_read, 1 do 
+  print(i)
+end;
