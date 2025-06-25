7c610d3c898b701087d04122a83735ac5a14de95
bheem-works <shokokomichan098@gmail.com>
2025-06-25 07:48:23 +0545

    somethingWronInTheLoops

--- test.lua
+++ test.lua
@@ -17,3 +17,13 @@ end
 name("vim")
 
 -- fabincii sequence 
+local function fabinci(n) 
+  local fabi = {0,1}
+  for i = 2, i < n , 1 do -- am i getting the n value?? (need to check the n value;)
+  fabi[i] = fabi[n - 1] + fabi[n - 2]   
+  end
+  return fabi;
+end
+
+fabinci(2)
+
