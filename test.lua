ecf81689a611a06dc4a782e9339f86e7f56e75ef
bheem-works <shokokomichan098@gmail.com>
2025-06-25 08:16:20 +0545

    fabinciiSequence

--- test.lua
+++ test.lua
@@ -17,13 +17,24 @@ end
 name("vim")
 
 -- fabincii sequence 
+-- Problem : how can i get the value like the 0,1, If i am having the parameter value of the 2; 
 local function fabinci(n) 
-  local fabi = {0,1}
-  for i = 2, i < n , 1 do -- am i getting the n value?? (need to check the n value;)
-  fabi[i] = fabi[n - 1] + fabi[n - 2]   
+  local fabi = {0,1} -- value store cha yesma; 
+  for i = 2, n-1 do -- am i getting the n value?? (need to check the n value;)
+  fabi[i + 1] = fabi[i] + fabi[i - 1]  -- Yo run executer vako cha kih nai??  
   end
   return fabi;
 end
 
-fabinci(2)
+local show_fabi = fabinci(2)
+for _,value in ipairs(show_fabi) do 
+  print(value)
+end
 
+-- local function param_value(n) 
+--   for i = 1, n do 
+--     print(i)
+--   end
+-- end
+
+-- param_value(3)
