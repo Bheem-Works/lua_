cbecb6d61a622ac4ec87b6f24ab4708b159954a7
bheem-works <shokokomichan098@gmail.com>
2025-06-29 13:04:52 +0545

    fileHandling

--- fileHandling.lua
+++ fileHandling.lua
@@ -3,11 +3,20 @@
 -- Set the output file to "mom.txt" (creates it if it doesn't exist)
 io.output("vim.txt")
 
+-- Huhu don't want to use the vs code but i am using the vs code because there is no any choice. 
 -- Write something to the file
-io.write("this is not the right time") -- it overwrite the file. 
-local file = io.read(5)
+io.write("this is not the right time okay vim?? man") -- it overwrite the file. 
+-- local file = io.read(5)
+local file = io.read("*all")
 -- Close the file
 io.close()
 
 print(file)
 
+-- File handling code by documentations.
+-- The methods of the file hanlding; 
+-- output() to make the file. 
+-- input() to enter the file. 
+-- read() to read the file.
+-- write() to write the file.
+-- close() to close the file. 
