b52100df6523d16982298e9632acd922da5110db
bheem-works <shokokomichan098@gmail.com>
2025-06-29 22:20:53 +0545

    osModule

--- osModule.lua
+++ osModule.lua
@@ -6,3 +6,25 @@ print(os.time({
     day = 3,
     hour = 13,
 })) -- to show the time. === it shows the time from that day to this date. 
+-- to show the difference between the past and the current time we use the => os.difftime(currentTime,pastTime)
+
+-- print(os.getenv("user"))
+-- print(os.execute("whoami"))
+-- print(os.execute("cls"))
+
+-- How to know how much the code take time to run 
+local start = os.clock();
+
+for i = 1, 100000000 do
+    local x = 10
+end
+
+print(os.clock() - start)
+
+-- about the os exit 
+for i = 1, 10, 1 do
+   print(i) 
+   if i == 5  then
+   os.exit() -- it will entirely exit os system.
+   end
+end
