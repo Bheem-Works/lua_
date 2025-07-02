e75bd51b679c57568fcf377995f475065cdf4145
bheem-works <shokokomichan098@gmail.com>
2025-07-02 23:15:41 +0545

    need a sleep

---
+++ oop.lua
@@ -0,0 +1,17 @@
+-- Oop --> object oriented programming language; 
+
+local object = {
+  name:"vim",
+  class:"twelver",
+  style:"alright"
+}
+
+local function Pet(name)
+  name = "tizero" or name
+  return {
+    nam:"kitty"
+  }
+end
+
+local cat = Pet("miso")
+print(cat.name)
