6651bcf64920976b294252a1a768069247037c88
bheem-works <shokokomichan098@gmail.com>
2025-07-03 23:02:56 +0545

    let's do this with the website documentaion

--- oop.lua
+++ oop.lua
@@ -2,6 +2,8 @@
 -- Oops main thing's is about the classes. 
 -- Note~lua is not the object oriented programming language okay.
 -- Meaning of the inherit --> It can acess the value to the one functions or the one class to the another classes.
+
+-- bujhekai chiana maile tah object oreinted programming language k ho yo? 
 local object = {
   name = "vim",
   class="twelver",
@@ -21,3 +23,15 @@ end
 local cat = Pet("miso")
 print(cat.name)
 cat:feed()
+
+local function dog(name,breed)
+ local dogUser = Pet(name)
+ dogUser.breed = "breed"
+--  calling the another functions inside the varaibles. 
+ local isLoyal = function (self)
+   print("wof woff")
+ end
+ return dog
+end
+
+local dogCall = dog("mizo","puppie")
