a77bcf19ec1cacd401c23725bbbf62d3f2326841
bheem-works <shokokomichan098@gmail.com>
2025-07-03 22:50:02 +0545

    ooppbasics

--- oop.lua
+++ oop.lua
@@ -1,6 +1,7 @@
 -- Oop --> object oriented programming language; 
 -- Oops main thing's is about the classes. 
-
+-- Note~lua is not the object oriented programming language okay.
+-- Meaning of the inherit --> It can acess the value to the one functions or the one class to the another classes.
 local object = {
   name = "vim",
   class="twelver",
@@ -10,9 +11,13 @@ local object = {
 local function Pet(name)
   name = "tizero" or name
   return {
-    name = "kitty"
+    name = "kitty is not right now okay man.",
+    feed = function (self)
+    self.name = "full"
+    end
   }
 end
 
 local cat = Pet("miso")
 print(cat.name)
+cat:feed()
