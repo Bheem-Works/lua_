e10ffe8b1d68ad83e36aaceecfe88856c318be3d
bheem-works <shokokomichan098@gmail.com>
2025-07-04 17:29:04 +0545

    oopBasics and the difference

--- oopTwo.lua
+++ oopTwo.lua
@@ -1,22 +1,8 @@
 -- This is the lua oop file from the original documenations haha. 
 
+-- Why the colons is used for? colons is the methods where it is used to modified and reference of own objects.
+-- : self objects pathaunu ra access grnu milcha. But '.' use grda chai self pathaunu mildaina.
 
-Rectangle = {area = 0, length = 0, breadth = 0}
+-- Here is the simple examples. 
 
-function Rectangle:new(o,length,breadth) 
-  o = ooooooo
-
-  setMetabale(o,self)
-  self.length = length or 0 
-  self.breadth = breadth or 0 
-  self.area = length * breadth;
-  return o
-end
-
-function Rectangle:printArea() 
-  print("This is the area",self.area)
-end
-
-r = Rectangle:new(nil,10,10)
-print(r.length)
-r:printArea()
+car = {}
