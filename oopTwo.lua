99efdcb19e32eddb48f050437f98a7a54c7e98a9
bheem-works <shokokomichan098@gmail.com>
2025-07-04 17:06:46 +0545

    oopMistakes

--- oopTwo.lua
+++ oopTwo.lua
@@ -1,8 +1,22 @@
 -- This is the lua oop file from the original documenations haha. 
 
-Account = {balance = 0}
-function Account.withDraw(v)
-  Account.balance = Account.balance - v
+
+Rectangle = {area = 0, length = 0, breadth = 0}
+
+function Rectangle:new(o,length,breadth) 
+  o = ooooooo
+
+  setMetabale(o,self)
+  self.length = length or 0 
+  self.breadth = breadth or 0 
+  self.area = length * breadth;
+  return o
 end
 
-Account.withDraw(300)
+function Rectangle:printArea() 
+  print("This is the area",self.area)
+end
+
+r = Rectangle:new(nil,10,10)
+print(r.length)
+r:printArea()
