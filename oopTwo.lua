a230373f14c4afb2ed38c33ba0d36ae95b0e17cd
bheem-works <shokokomichan098@gmail.com>
2025-07-05 09:43:30 +0545

    principle and the build methods of the oops

--- oopTwo.lua
+++ oopTwo.lua
@@ -1,26 +1,22 @@
+-- What is the object oriented programming language ? 
+-- => Object oriented programming language means this is the pattern 
+-- that is build around the build around the objects and the entities.
+
+-- Oops building blocks; 
+-- 1. classes
+-- 2. object
+-- 3. methods
+-- 4. attributes
+--
+--
+-- Principles of the oop 
+-- 1. Encapsulations
+-- 2. Abstractions
+-- 3. Inhertance
+-- 4. Polymorphism
+
 -- Why the colons is used for? colons is the methods where it is used to modified and reference of own objects.
 -- : self objects pathaunu ra access grnu milcha. But '.' use grda chai self pathaunu mildaina.
-
--- Here is the simple examples. 
-
--- _G. car = {}
-
--- function car:new(brand)
---   local car = {car == car}
---   setmetatable(self,car)
---   self._index = self
---   self.brand = brand
---   return car
--- end
-
--- function car:drive()
---   print("I am driving this ",self.brand)
--- end
-
--- local myCar = car:new("bugati")
--- myCar:drive()
-
-
 
 
 local savedName = {
