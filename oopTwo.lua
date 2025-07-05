3074db13ee4f8f87bd5e737733a0732ca25b95a7
bheem-works <shokokomichan098@gmail.com>
2025-07-05 09:12:24 +0545

    luaOop

--- oopTwo.lua
+++ oopTwo.lua
@@ -1,22 +1,45 @@
--- This is the lua oop file from the original documenations haha. 
-
 -- Why the colons is used for? colons is the methods where it is used to modified and reference of own objects.
 -- : self objects pathaunu ra access grnu milcha. But '.' use grda chai self pathaunu mildaina.
 
 -- Here is the simple examples. 
 
-car = {}
+-- _G. car = {}
 
-function car:new(brand)
-  local obj = {car == car}
-  setMetatable(obj,self)
-  self._index = self
-  return obj
+-- function car:new(brand)
+--   local car = {car == car}
+--   setmetatable(self,car)
+--   self._index = self
+--   self.brand = brand
+--   return car
+-- end
+
+-- function car:drive()
+--   print("I am driving this ",self.brand)
+-- end
+
+-- local myCar = car:new("bugati")
+-- myCar:drive()
+
+
+
+
+local savedName = {
+  name = "juillet",
+  class = "don'w know",
+  status = "poor",
+  obj = {
+mobile = "andriod4"
+  }
+}
+
+function Pet(name)
+  name = name or "miso"
+  return {
+    name = name
+  }
 end
 
-function car:drive() 
-  print("i am driving this ",self.brand)
-end
-
-local myCar = car:new("toyata")
-myCar:drive();
+local callName = savedName;
+local callPet = Pet()
+print(callPet.name)
+print(callName.class)
